c6c9b8b715dab8b6ba6b14eec81b870302f7406b
Kevinobote <omoshk49@gmail.com>
2026-06-23 10:49:50 +0300

    add testimonials section back to main page

--- frontend/src/pages/main_page.jsx
+++ frontend/src/pages/main_page.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero";
 import CTASection from "../components/CTA";
+import Testimonials from "../components/Testimonials";
 
 const Main = () => {
   return (
     <div className="min-h-screen bg-offwhite dark:bg-slate-900">
       <Hero />
+      <Testimonials />
       <CTASection />
     </div>
   );
